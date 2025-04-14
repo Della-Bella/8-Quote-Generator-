2a742e8acf8556088738b1c53d9fb0825445aa72
Della Bella <gisdellabella@uol.com.br>
2025-04-14 18:29:20 +0100

    Initial project setup

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+const quote = document.getElementById("quoteRandom"); //p= quote
+
+const authorQuote = document.getElementById("author"); //p= author
+
+const btnQuote = document.getElementById("newQuote"); // btn = newquote
+
+const twitter= document.getElementById("twitter"); //btn= twitter
